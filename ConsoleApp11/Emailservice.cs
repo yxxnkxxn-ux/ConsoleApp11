62ab69cdbb93b024bdc38bf05241f292f753643b
yoonkwon <yxxnkxxn@yonsei.ac.kr>
2025-11-16 16:31:52 +0900

    초기 커밋.

---
+++ ConsoleApp11/Emailservice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace AssignmentNotifier
+{
+    public class EmailService
+    {
+        private string _email;
+        private string _password;
+
+        public EmailService(string email, string password)
+        {
+            _email = email;
+            _password = password;
+        }
+
+        public void SendMail(string toEmail, string subject, string body)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(_email, _password);
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(_email);
+                mail.To.Add(toEmail);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                client.Send(mail);
+
+                Console.WriteLine("이메일 전송 완료!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("이메일 전송 실패: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleApp11/models/taskitem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace AssignmentNotifier.Models
+{
+    public class TaskItem
+    {
+        public string Title { get; set; }
+        public string Memo { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public int DaysLeft()
+        {
+            return (DueDate.Date - DateTime.Now.Date).Days;
+        }
+
+        public bool IsDueTomorrow()
+        {
+            return DaysLeft() == 1;
+        }
+    }
+}
